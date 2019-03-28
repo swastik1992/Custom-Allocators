72dcb1a0847ec5f020088353be2fcb8398866b47
swastik1992 <swastikswarnkar@gmail.com>
2019-03-28 23:51:12 +0800

    Initial Setup for project.

---
+++ CustomAllocatorsCollection/CustomAllocatorsCollection/CustomAllocatorsCollection.cpp
@@ -0,0 +1,24 @@
+// CustomAllocatorsCollection.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+///////////////////////////////////////////
+//	Linear Allocator				[]
+//	Stack Allocator					[]
+//  Double Ended Stack Allocator	[]
+//	Pool Allocator					[]
+//	Free List Allocator				[]
+//	Aligned Allocator				[]
+//	Single Buffered Allocator		[]
+//	Double Buffered Allocator		[]
+/////////////////////////////////////////
+
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+
